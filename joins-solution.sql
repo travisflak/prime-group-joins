b8fd809318d477278c0d86b80e9532f53d447dac
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-11-03 16:03:15 -0600

    created DB warehouse, and created a repo and sql file with the 8 problems to solve

---
+++ joins-solution.sql
@@ -0,0 +1,15 @@
+--1. Get all customers and their addresses.
+
+--2. Get all orders and their line items (orders, quantity and product).
+
+--3. Which warehouses have cheetos?
+
+--4. Which warehouses have diet pepsi?
+
+--5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+
+--6. How many customers do we have?
+
+--7. How many products do we carry?
+
+--8. What is the total available on-hand quantity of diet pepsi?
