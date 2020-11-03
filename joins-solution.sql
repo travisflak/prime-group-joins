c6c00757fddc234cff2a520c87bb312be8a1a6b2
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-11-03 16:21:56 -0600

    solved 1

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,4 +1,7 @@
 --1. Get all customers and their addresses.
+SELECT * FROM "customers"
+JOIN "addresses"
+ON "customers"."id" = "addresses"."id";
 
 --2. Get all orders and their line items (orders, quantity and product).
 
